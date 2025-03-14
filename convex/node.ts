4e7d9f08ed1f1e0682ba19999a2ef80f6751a517
Ian Macartney <ianhmacartney@gmail.com>
2025-03-13 21:00:45 -0700

    add some imports to test node

--- convex/node.ts
+++ convex/node.ts
@@ -1,11 +1,15 @@
 "use node";
-import { Mastra } from "@mastra/core";
+import { Mastra } from "../mastra-clone/packages/core/src/mastra/index";
+import { Agent } from "../mastra-clone/packages/core/src/agent/index";
 import { action } from "./_generated/server";
-
+import { Step } from "../mastra-clone/packages/core/src/workflows/step";
+import { Memory } from "../mastra-clone/packages/memory/src/index";
 export const a = action({
   args: {},
   handler: async (ctx, args) => {
-    const mastra = new Mastra();
+    new Mastra({} as any);
+    new Agent({} as any);
+    new Step({} as any);
+    new Memory({} as any);
   },
 });
-export const mastra = new Mastra();
