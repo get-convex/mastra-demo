e1a876cdb4e8f0fcfef2336bb1ef48255f22805c
Ian Macartney <ianhmacartney@gmail.com>
2025-03-18 16:04:23 -0700

    snip schema for now

--- convex/_.ts
+++ convex/_.ts
@@ -1,7 +1,2 @@
 "use node";
 export * as libsql from "@libsql/client";
-import { TABLE_NAMES } from "./schema";
-import { TABLE_NAMES as STORAGE_TABLE_NAMES } from "@mastra/core/storage";
-
-const _typeAssertion: STORAGE_TABLE_NAMES = "" as STORAGE_TABLE_NAMES;
-const _otherAssertion: TABLE_NAMES = "" as STORAGE_TABLE_NAMES;
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,18 +1,6 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-const TABLE_WORKFLOW_SNAPSHOT = "mastra_workflow_snapshot";
-const TABLE_EVALS = "mastra_evals";
-const TABLE_MESSAGES = "mastra_messages";
-const TABLE_THREADS = "mastra_threads";
-const TABLE_TRACES = "mastra_traces";
-export type TABLE_NAMES =
-  | typeof TABLE_WORKFLOW_SNAPSHOT
-  | typeof TABLE_EVALS
-  | typeof TABLE_MESSAGES
-  | typeof TABLE_THREADS
-  | typeof TABLE_TRACES;
-
 // The schema is entirely optional.
 // You can delete this file (schema.ts) and the
 // app will continue to work.
@@ -21,55 +9,4 @@ export default defineSchema({
   numbers: defineTable({
     value: v.number(),
   }),
-  [TABLE_WORKFLOW_SNAPSHOT]: defineTable({
-    workflow_name: v.string(),
-    run_id: v.string(),
-    snapshot: v.string(),
-    createdAt: v.number(),
-    updatedAt: v.number(),
-  }),
-  [TABLE_EVALS]: defineTable({
-    input: v.string(),
-    output: v.string(),
-    result: v.any(),
-    agent_name: v.string(),
-    metric_name: v.string(),
-    instructions: v.string(),
-    test_info: v.optional(v.any()),
-    global_run_id: v.string(),
-    run_id: v.string(),
-    created_at: v.number(),
-  }),
-  [TABLE_MESSAGES]: defineTable({
-    id: v.string(),
-    thread_id: v.string(),
-    content: v.string(),
-    role: v.string(),
-    type: v.string(),
-    createdAt: v.number(),
-  }).index("id", ["id"]),
-  [TABLE_THREADS]: defineTable({
-    id: v.string(),
-    resourceId: v.string(),
-    title: v.string(),
-    metadata: v.optional(v.string()),
-    createdAt: v.number(),
-    updatedAt: v.number(),
-  }).index("id", ["id"]),
-  [TABLE_TRACES]: defineTable({
-    id: v.string(),
-    parentSpanId: v.optional(v.string()),
-    name: v.string(),
-    traceId: v.string(),
-    scope: v.string(),
-    kind: v.number(),
-    attributes: v.optional(v.any()),
-    status: v.optional(v.any()),
-    events: v.optional(v.any()),
-    links: v.optional(v.any()),
-    other: v.optional(v.string()),
-    startTime: v.int64(),
-    endTime: v.int64(),
-    createdAt: v.number(),
-  }).index("id", ["id"]),
 });
